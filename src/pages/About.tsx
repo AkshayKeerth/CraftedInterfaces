ba31a5b4bde759a7dfa19ad1468c70ebc7ce4385
Akshay Keerth <154725222+AkshayKeerth@users.noreply.github.com>
2025-06-01 15:52:57 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -170,8 +170,9 @@ const About = () => {
               <div className="glass rounded-2xl overflow-hidden h-[500px] relative">
                 {/* Replace with actual image */}
                 <div 
-                  className="absolute inset-0 bg-cover bg-center" 
-                  style={{ backgroundImage: "url(https://i.postimg.cc/cC6yVbjm/headshotmaster-image-1744448281432.png)"}}
+                  className="absolute inset-0 bg-cover bg-center"
+                  {/* Original Image: https://i.postimg.cc/cC6yVbjm/headshotmaster-image-1744448281432.png */}
+                  style={{ backgroundImage: "url(https://i.postimg.cc/JnccpZtg/446eb15cc10940b084b6c5da79b73123-0-1748771860.png)"}}
                 ></div>
               </div>
             </div>
